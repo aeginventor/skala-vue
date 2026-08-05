ecf1c24bd92195d23d9038268abe1ee46383741d
김산 <aeginventor@gmail.com>
2026-08-05 10:51:36 +0900

    fix: '소개' 버튼을 헤더 타이틀 옆으로 이동

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,6 @@ const route = useRoute()
         <h1>
           <span class="brand-badge"><i class="fa-solid fa-cloud-sun"></i></span>
           날씨 Dock
-        </h1>
-        <div class="page-header__actions">
-          <!--
-            [피드백 반영] Dock 맨 끝까지 스크롤해야 나오던 "소개" 링크를, 화씨/섭씨
-            버튼 바로 왼쪽에 항상 보이는 버튼으로 하나 더 뒀다. 누르기 편하게
-            텍스트까지 같이 넣어서 UnitToggler 버튼보다 살짝 더 크게 잡았다.
-          -->
           <RouterLink
             to="/about"
             class="about-btn ink-pressable"
@@ -27,6 +20,8 @@ const route = useRoute()
           >
             <i class="fa-solid fa-circle-info"></i> 소개
           </RouterLink>
+        </h1>
+        <div class="page-header__actions">
           <UnitToggler />
         </div>
       </div>
